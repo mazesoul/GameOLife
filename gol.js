2bf4090e1703728f10c6548085e66d360b3cd3d4
mazesoul <mazesoul@gmail.com>
2012-03-29 01:26:53 -0400

    Added visualization to string and creation from string. Added glider translation test

--- gol.js
+++ gol.js
@@ -1,95 +1,213 @@
-
-var gol = function (global) {
-
+!function () {
   "use strict";
 
-  var fromDead  = [ 0,0,0,1 ] // Dead  [0] rules
-    , fromAlive = [ 0,0,1,1 ] // Alive [1] rules
+  var gol = function (global) {
 
-  function cellToCoords (cell) {
-    return cell.split(',').map(Number)
-  }
+    var fromDead  = [ 0,0,0,1 ] // Dead  [0] rules
+      , fromAlive = [ 0,0,1,1 ] // Alive [1] rules
 
-  // function populationToCoords (population) {
-  //   return Object.keys(population).map(cellToCoords)
-  // }
+    function cellToCoords (cell) {
+      return cell.split(',').map(Number)
+    }
 
-  function neighbors (cell) {
-    var coords = cellToCoords(cell)
+    function neighbors (cell) {
+      var coords = cellToCoords(cell)
 
-    // Memoize
-    return  [ [coords[0]-1, coords[1]-1].toString()
-            , [coords[0]-1, coords[1]  ].toString()
-            , [coords[0]-1, coords[1]+1].toString()
-            , [coords[0]  , coords[1]-1].toString()
-            , [coords[0]  , coords[1]+1].toString()
-            , [coords[0]+1, coords[1]-1].toString()
-            , [coords[0]+1, coords[1]  ].toString()
-            , [coords[0]+1, coords[1]+1].toString()
-            ]
-  }
+      // Memoize
+      return  [ [coords[0]-1, coords[1]-1].toString()
+              , [coords[0]-1, coords[1]  ].toString()
+              , [coords[0]-1, coords[1]+1].toString()
+              , [coords[0]  , coords[1]-1].toString()
+              , [coords[0]  , coords[1]+1].toString()
+              , [coords[0]+1, coords[1]-1].toString()
+              , [coords[0]+1, coords[1]  ].toString()
+              , [coords[0]+1, coords[1]+1].toString()
+              ]
+    }
 
 
 
-  function next ( population ) {
+    function next ( population ) {
 
-    var _next     = {} // The _next generation
-      , deadites  = {} // The dead neighbors encountered
+      var _next     = {} // The _next generation
+        , deadites  = {} // The dead neighbors encountered
 
-    // Run all the living cell
-    Object.keys(population).forEach(function (cell) {
-      var alive = 0
+      // Run all the living cell
+      Object.keys(population).forEach(function (cell) {
+        var alive = 0
 
-      neighbors(cell).forEach(function (cell) {
-        cell in population ? alive++
-                           : deadites[cell] = false // Deadcell, schedule for visit
+        neighbors(cell).forEach(function (cell) {
+          cell in population ? alive++
+                             : deadites[cell] = false // Deadcell, schedule for visit
+        })
+        // Apply to the _next cell
+        if (fromAlive[alive] ) _next[cell] = true;
       })
-      // Apply to the _next cell
-      if (fromAlive[alive] ) _next[cell] = true;
+
+      // Run all the deadites
+      Object.keys(deadites).forEach(function (cell) {
+        var livings = neighbors(cell).filter(function (cell) { return population[cell] })
+        if (fromDead[livings.length]) _next[cell] = true;
+      })
+
+      return _next;
+
+
+    }
+
+    return  { next : next}
+
+  }();
+
+
+  function assertPopulation ( population, c1, c2 /*,...*/) {
+    [].slice.call(arguments,1).forEach(function (cell) {
+      console.assert(population[cell], 'Missing : ' + cell)
+    })
+  }
+
+  function strToPopulation ( r1, r2 /*,rn*/) {
+    var population = {}
+      , x
+      , y = arguments.length
+      , row;
+
+    while (y--) {
+      x = arguments[y].length
+      while (x--)
+        if ( 'O' === arguments[y][x] ) population[[x,y].toString()] = true;
+
+    }
+
+    return population
+  }
+
+  console.log("Population :: ", strToPopulation( "O.."
+                                               , ".O."
+                                               , "..O"
+                                               ))
+
+  // Assumes population are alway in positive space
+  function populationToStr (population) {
+    var rows = [[]]
+    Object.keys(population).forEach(function (cell) {
+      var p = cell.split(',').map(Number)
+      rows[p[1]] = rows[p[1]] || [];
+      rows[p[1]][p[0]] = true;
     })
 
-    // Run all the deadites
-    Object.keys(deadites).forEach(function (cell) {
-      var livings = neighbors(cell).filter(function (cell) { return population[cell] })
-      if (fromDead[livings.length]) _next[cell] = true;
-    })
+    var max = rows.reduce(function (max, r,i) {
+                            return r.length > max ? r.length : max;
+                          }, 0)
+    //rows.forEach(function (r) {r.length = max})
+    var row
+      , r = rows.length
+      , c
+    while (r--) {
+      row = (rows[r] = rows[r] || [])
+      c = (row.length = max)
 
-    return _next;
-
+      while(c--) row[c] = row[c] ? 'O' : '.' ;
+      rows[r] = row.join('')
+    }
+    return rows.join('\n')
 
   }
 
-  return  { next : next}
-
-}();
-
-
-// Create a game population
-var population = gol.next( { '1,0' : true
-                           , '1,1' : true
-                           , '1,2' : true
-                           }
-                         );
-
-console.log(population)
-var assert = require('assert')
-// [1,0]
-// [1,1]
-// [1,2]
-
-console.assert(population['0,1'], 'Missing : 0,1')
-console.assert(population['1,1'], 'Missing : 1,1')
-console.assert(population['2,1'], 'Missing : 2,1')
-
-population = gol.next(population)
-
-console.assert(population['1,0'], 'Missing : 1,0')
-console.assert(population['1,1'], 'Missing : 1,1')
-console.assert(population['1,2'], 'Missing : 1,2')
+  console.log("str Population :: \n", populationToStr( { '0,1' : 1
+                                                       , '0,3' : 1
+                                                       , '1,0' : 1
+                                                       , '2,1' : 1
+                                                       }
+                                                     ))
 
 
+  // Create a game population
+  ;!function testBlinker () {
+    // From      to
+    // .O.       ...
+    // .O.   x1  OOO
+    // .O.       ...
+    var population = gol.next( { '1,0' : true
+                               , '1,1' : true
+                               , '1,2' : true
+                               }
+                             );
 
+    assertPopulation(population, '0,1', '1,1', '2,1')
+
+    population = gol.next(population)
+
+    assertPopulation(population, '1,0', '1,1', '1,2')
+
+    console.log('Blinker assertion done!');
+  }()
 
 
 
+  ;!function testGlidderTranslation () {
+    // From       To
+    // ....       OOO.
+    // .OOO       O...
+    // .O..  x4   .O..
+    // ..O.       ....
 
+    var population = strToPopulation( '....'
+                                    , '.OOO'
+                                    , '.O..'
+                                    , '..O.'
+                                    )
+
+    console.log('Start ====================')
+    console.log('\n%s',populationToStr(population))
+    population = gol.next(population)
+    console.log('\n',populationToStr(population))
+    population = gol.next(population)
+    console.log('\n%s',populationToStr(population))
+    population = gol.next(population)
+    console.log('\n%s',populationToStr(population))
+    population = gol.next(population)
+    console.log('\n%s',populationToStr(population))
+
+    assertPopulation( population
+                    , '0,0' ,'1,0' ,'2,0'
+                    , '0,1'   /*  */  /*  */
+                      /*  */,'1,2'   /*  */
+
+                    )
+
+
+    console.log('Glidder translation done!');
+  }()
+
+  ;!function testGlidderTranslation () {
+    "use strict";
+    // From       To
+    // ....       OOO.
+    // .OOO       O...
+    // .O..  x4   .O..
+    // ..O.       ....
+
+    var population = { '0,0' :1,'1,0' :1,'2,0' :1
+                     , '0,1' :1
+                               ,'1,2' :1
+                     }
+
+
+    population = gol.next(population)
+    population = gol.next(population)
+    population = gol.next(population)
+    population = gol.next(population)
+
+    assertPopulation( population
+                    , '-1,-1' ,'0,-1' ,'1,-1'
+                    , '-1,0'   /*  */  /*  */
+                      /*  */  ,'0,1'   /*  */
+
+                    )
+
+
+    console.log('Glidder translation done!');
+  }()
+
+}()
